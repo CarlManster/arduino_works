3f20bc8b26014381c8cb591c741c22b9ee5cc647
CarlManster <hypermanster@gmail.com>
2016-10-08 17:42:21 +0900

    encoder
    1. use readString() instead of read()
    2. piezo code changed to use tone() instead of analogWrite()

--- works/sketch_oct04b_encoder/sketch_oct04b_encoder.ino
+++ works/sketch_oct04b_encoder/sketch_oct04b_encoder.ino
@@ -1,8 +1,6 @@
-int inputPin = 0;
 int ledPin = 13;
 int piezoPin = 9;
-int piezoValueOn = 127;
-int piezoValueOff = 0;
+int piezoFrequency = 6000;
 int dotDelay = 200;
 
 char* letters[] = { 
@@ -10,11 +8,10 @@ char* letters[] = {
   "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." 
  };
 char* numbers[] = { "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----." };
-char strings[] = { };
-char* morses[] = { };
 
 void workSequence(char* sequence) {
   int i = 0;
+
   while (sequence[i] != NULL) {
     workDotOrDash(sequence[i]);
     i++;
@@ -24,19 +21,19 @@ void workSequence(char* sequence) {
 
 void workDotOrDash(char dotOrDash) {
   digitalWrite(ledPin, HIGH);
-  analogWrite(piezoPin, piezoValueOn);
+  
   if (dotOrDash == '.') {
+    tone(piezoPin, piezoFrequency, dotDelay);
     delay(dotDelay);
   } else {
+    tone(piezoPin, piezoFrequency, dotDelay * 3);
     delay(dotDelay * 3);
   }
   digitalWrite(ledPin, LOW);
-  analogWrite(piezoPin, piezoValueOff);
   delay(dotDelay);
 }
 
 void setup() {
-  pinMode(inputPin, INPUT);
   pinMode(ledPin, OUTPUT);
   pinMode(piezoPin, OUTPUT);
   
@@ -44,36 +41,39 @@ void setup() {
 }
 
 void loop() {
-  int i = 0;
-  
-  while (Serial.available() > 0) {
-    char ch = Serial.read();
-    char* sequence = NULL;
+  if (Serial.available() > 0) {
+    String str = Serial.readString();
 
-    if (ch >= 'a' && ch <= 'z') {
-      sequence = letters[ch - 'a'];
-    } else if (ch >= 'A' && ch <= 'Z') {
-      sequence = letters[ch - 'A'];
-    } else if (ch >= '0' && ch <= '9') {
-      sequence = numbers[ch - '0'];
-    } else if (ch == ' ') {
-      delay(dotDelay * 4);
+    Serial.println(str);
+
+    String morses = "";
+    
+    for (int i = 0 ; i < str.length(); i++) {
+      char ch = str.charAt(i);
+      char* sequence = NULL;
+
+      if (ch >= 'a' && ch <= 'z') {
+        sequence = letters[ch - 'a'];
+      } else if (ch >= 'A' && ch <= 'Z') {
+        sequence = letters[ch - 'A'];
+      } else if (ch >= '0' && ch <= '9') {
+        sequence = numbers[ch - '0'];
+      } else if (ch == ' ') {
+        delay(dotDelay * 4);
+      }
+
+      if (sequence != NULL) {
+        Serial.print(ch);
+        Serial.print(" : ");
+        Serial.println(sequence);
+
+        workSequence(sequence);
+        morses += String(sequence) + " ";
+      } else {
+        morses += " ";
+      }
     }
 
-    if (sequence != NULL) {
-      workSequence(sequence);
-      morses[i] = sequence;
-    } else {
-      morses[i] = " ";
-    }
-
-    strings[i] = ch;
-
-    i++;
-  }
-
-  Serial.println(strings);
-  for (int j = 0; j < sizeof(morses); j++) {
-    Serial.println(morses[j]);
+    Serial.println(morses);
   }
 }
